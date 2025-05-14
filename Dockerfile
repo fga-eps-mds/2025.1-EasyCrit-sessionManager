10eb078559aebd594ef3c798355a283d07f04beb
KessJhones <tavaresjhones14@gmail.com>
2025-05-14 17:53:08 -0300

    feat: Adiciona estrutura inicial do projeto

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12-alpine
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./app ./app
+
+COPY .env .env
+
+EXPOSE ${SESSIONMANAGER_PORT}
---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# adicionar CORS
+origins = "*" # Alterar para dominios específicos em produção
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Bem vindo à API de controle de sessão!"}
