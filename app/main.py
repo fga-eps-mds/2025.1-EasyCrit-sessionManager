e21672272f5cec864e7e8fc44d340e92e102fe6b
Thales Duarte <thalesduarte05@gmail.com>
2025-05-23 23:41:04 -0300

    refactor: reformatando o código.

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
 
 app.add_middleware(JWTAuthMiddleware)
 
+
 @app.get('/')
 def read_root():
   return {'message': 'Bem vindo à API de controle de sessão!'}
--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Request, HTTPException
+from fastapi import Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from starlette.middleware.base import BaseHTTPMiddleware
 import jwt
@@ -6,34 +6,34 @@ from jwt import ExpiredSignatureError, InvalidTokenError
 import os
 from fastapi.responses import JSONResponse
 
-SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
+
 
 class JWTAuthMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app):
-        super().__init__(app)
-        self.require_auth = [
-            ("GET", "/users"),
-            ("PUT", "/users"),
-            ("PATCH", "/users"),
-        ]
-        self.security = HTTPBearer()
+  def __init__(self, app):
+    super().__init__(app)
+    self.require_auth = [
+      ('GET', '/users'),
+      ('PUT', '/users'),
+      ('PATCH', '/users'),
+    ]
+    self.security = HTTPBearer()
 
-    async def dispatch(self, request: Request, call_next):
-        needs_auth = any(
-            request.method == method and request.url.path.startswith(path)
-            for method, path in self.require_auth
-        )
+  async def dispatch(self, request: Request, call_next):
+    needs_auth = any(
+      request.method == method and request.url.path.startswith(path) for method, path in self.require_auth
+    )
 
-        if needs_auth:
-            try:
-                credentials: HTTPAuthorizationCredentials = await self.security(request)
-                payload = jwt.decode(credentials.credentials, SECRET_KEY, algorithms=["HS256"])
-                request.state.user = payload
-            except ExpiredSignatureError:
-                return JSONResponse("Expired token", status_code=401)
-            except InvalidTokenError:
-                return JSONResponse("Invalid token", status_code=401)
-            except Exception:
-                return JSONResponse("Unauthorizad token", status_code=401)
+    if needs_auth:
+      try:
+        credentials: HTTPAuthorizationCredentials = await self.security(request)
+        payload = jwt.decode(credentials.credentials, SECRET_KEY, algorithms=['HS256'])
+        request.state.user = payload
+      except ExpiredSignatureError:
+        return JSONResponse('Expired token', status_code=401)
+      except InvalidTokenError:
+        return JSONResponse('Invalid token', status_code=401)
+      except Exception:
+        return JSONResponse('Unauthorizad token', status_code=401)
 
-        return await call_next(request)
+    return await call_next(request)
