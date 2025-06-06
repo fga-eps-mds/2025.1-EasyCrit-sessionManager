0cff4264e9405b6ef0fbcd1a8dff72f8654e7005
Luara Cristiana <cristianaluara@gmail.com>
2025-06-06 16:10:25 -0300

    fix: Apply formatting

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from app.websocket import router as websocket_router
 
@@ -10,15 +9,15 @@ app = FastAPI()
 origins = '*'  # Alterar para dominios específicos em produção
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
 )
 
 # http://127.0.0.1:8000/static/websocket_test.html
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount('/static', StaticFiles(directory='app/static'), name='static')
 
 # includes the router websocket
 app.include_router(websocket_router)
@@ -26,4 +25,4 @@ app.include_router(websocket_router)
 
 @app.get('/')
 def read_root():
-    return {'message': 'Bem vindo à API de controle de sessão!'}
+  return {'message': 'Bem vindo à API de controle de sessão!'}
--- app/websocket/router.py
+++ app/websocket/router.py
@@ -1,27 +1,25 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from typing import Optional
 from .connection_manager import manager
 import json
 
 router = APIRouter()
 
-@router.websocket("/ws/connect/{client_id}")
+
+@router.websocket('/ws/connect/{client_id}')
 async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await manager.connect(websocket, client_id)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            try:
-                message_data = json.loads(data)
-                
-                await manager.send_personal_message(f"Message text was: {message_data}", client_id)
-                
-                await manager.broadcast(
-                    f"Client {client_id}: {message_data}",
-                    exclude=client_id)
-                
-            except json.JSONDecodeError:
-                await manager.send_personal_message("Error: Invalid JSON", client_id)
-    except WebSocketDisconnect:
-        manager.disconnect(client_id)
-        await manager.broadcast(f"Client {client_id} left the chat")
+  await manager.connect(websocket, client_id)
+  try:
+    while True:
+      data = await websocket.receive_text()
+      try:
+        message_data = json.loads(data)
+
+        await manager.send_personal_message(f'Message text was: {message_data}', client_id)
+
+        await manager.broadcast(f'Client {client_id}: {message_data}', exclude=client_id)
+
+      except json.JSONDecodeError:
+        await manager.send_personal_message('Error: Invalid JSON', client_id)
+  except WebSocketDisconnect:
+    manager.disconnect(client_id)
+    await manager.broadcast(f'Client {client_id} left the chat')
