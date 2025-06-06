c9dd96e4e1856deefe0780dae8ccc525019ae014
Luara Cristiana <cristianaluara@gmail.com>
2025-06-06 15:45:29 -0300

    feat: Create websocket connection route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from app.websocket import router as websocket_router
 
 app = FastAPI()
 
@@ -7,14 +10,20 @@ app = FastAPI()
 origins = '*'  # Alterar para dominios específicos em produção
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=['*'],
-  allow_headers=['*'],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
+
+# http://127.0.0.1:8000/static/websocket_test.html
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# includes the router websocket
+app.include_router(websocket_router)
 
 
 @app.get('/')
 def read_root():
-  return {'message': 'Bem vindo à API de controle de sessão!'}
+    return {'message': 'Bem vindo à API de controle de sessão!'}
---
+++ app/websocket/__init__.py
@@ -0,0 +1,4 @@
+from .router import router
+from .connection_manager import manager
+
+__all__ = ['router', 'manager']
