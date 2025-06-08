1ce7e7832ee43918fd956258b115bf686fa3c1c4
uwatanabev <bernardowvenzi@gmail.com>
2025-06-07 21:29:14 -0300

    fix: Apply formatting

--- app/database/database.py
+++ app/database/database.py
@@ -3,28 +3,28 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
+  DATABASE_URL,
+  connect_args={'check_same_thread': False},
 )
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
+  autocommit=False,
+  autoflush=False,
+  bind=engine,
 )
 
 Base = declarative_base()
 
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
 
 
 def create_tables():
-    Base.metadata.create_all(bind=engine)
+  Base.metadata.create_all(bind=engine)
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,6 @@ client = TestClient(app)
 
 
 def test_read_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Bem vindo à API de controle de sessão!"}
+  response = client.get('/')
+  assert response.status_code == 200
+  assert response.json() == {'message': 'Bem vindo à API de controle de sessão!'}
